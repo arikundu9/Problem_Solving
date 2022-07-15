7f68cd344dfa68b1b906f066dfebdd85aa242593
Arijit Kundu <arikundu9@gmail.com>
2022-07-15 09:37:36 +0530

    leetcode added and roman started

---
+++ romanToInt.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define I 1
+#define V 5
+#define X 10
+#define L 50
+#define C 100
+#define D 500
+#define M 1000
+
+class Solution {
+ public:
+  Solution() {}
+  int romanToInt(string s) {
+    for (char c : s)
+    {
+        cout<<c<<endl;
+    }
+    return 1;
+  }
+};
+int main() {
+  Solution leetcode;
+  leetcode.romanToInt("");
+  return 0;
+}
