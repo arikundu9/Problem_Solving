7748c42ab8e98baa671f42c2154599b903a04307
Arijit Kundu <arikundu9@gmail.com>
2023-09-14 15:36:11 +0530

    6

---
+++ 6.Zigzag_Conversion.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+    Solution() {}
+    string convert(string s,int numRows) {
+        string r="PAHNAPLSIIGYIR";
+        return r;
+    }
+};
+int main() {
+    Solution leetcode;
+    string ip;
+    int numRows;
+    cin >> ip;
+    cin >> numRows;
+    cout<<leetcode.convert(ip,numRows);
+    return 0;
+}
