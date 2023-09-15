39d39833aedafc99f513a41cb8f352e3afa05359
Arijit Kundu <arikundu9@gmail.com>
2023-09-15 14:24:57 +0530

    ufff

--- 11.Container_With_Most_Water.cpp
+++ 11.Container_With_Most_Water.cpp
@@ -4,23 +4,65 @@ using namespace std;
 class Solution {
     public:
     Solution() {}
-    int maxArea(vector<int>& height) {
-        //Complexity: O(n^2)
+    int maxArea(vector<int>& h) {
+        //Complexity:
         int a=0;
-        for (int i = 0; i < height.size(); i++)
-        {
-            for (int j = 0; j < height.size(); j++)
-            {
-                if(j<i){
-                    int newA = (i-j)*min(height[i],height[j]);
-                    if(newA>a){
-                        a = newA;
-                    }
-                }
+        // cout<<"f="<<isHeightPrecedencedData(h);
+        // cout<<"lastSecHeighest="<<lastSecHeighest<<endl;
+        // if(isHeightPrecedencedData(h)){
+            int firstHeighest = posOf1st(heighest(h),h);
+            int lastHeighest = posOfLast(heighest(h),h);
+            int firstSecHeighest = posOf1st(secondHeighest(h),h);
+            int lastSecHeighest = posOfLast(secondHeighest(h),h);
+            if((lastSecHeighest - firstHeighest) >= (lastHeighest - firstSecHeighest)){
+                a = (lastSecHeighest - firstHeighest)*secondHeighest(h);
             }
-            
-        }
+            else{
+                a = (lastHeighest - firstSecHeighest)*secondHeighest(h);
+            }
+        // }
         return a;
+    }
+
+    bool isHeightPrecedencedData(vector<int>& h){
+        return h.size() <= secondHeighest(h);
+    }
+
+    int heighest(vector<int>& h){
+        int m = 0;
+        for(int x : h){
+            m = x > m ? x : m;
+        }
+        return m;
+    }
+
+    int secondHeighest(vector<int>& h){
+        int m = heighest(h) , sM = -1;
+        for(int x : h){
+            sM = x > sM and x < m ? x : sM;
+        }
+        return sM==-1 ? m : sM;
+    }
+
+    int posOf1st(int v,vector<int>& h){
+        int p=-1;
+        for(int i=0; i < h.size(); ++i){
+            if(h[i]==v){
+                p=i;
+                break;
+            }
+        }
+        return p;
+    }
+    int posOfLast(int v,vector<int>& h){
+        int p=-1;
+        for(int i=h.size() -1; i >= 0; --i){
+            if(h[i]==v){
+                p=i;
+                break;
+            }
+        }
+        return p;
     }
 };
 int main() {
