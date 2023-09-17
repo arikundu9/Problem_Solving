df65c26ffbb8ed17d8916b7fb2609ca4c487c2bf
Arijit Kundu <arikundu9@gmail.com>
2023-09-17 18:58:29 +0530

    DDDDD

--- 11.Container_With_Most_Water.cpp
+++ 11.Container_With_Most_Water.cpp
@@ -5,64 +5,21 @@ class Solution {
     public:
     Solution() {}
     int maxArea(vector<int>& h) {
-        //Complexity:
-        int a=0;
-        // cout<<"f="<<isHeightPrecedencedData(h);
-        // cout<<"lastSecHeighest="<<lastSecHeighest<<endl;
-        // if(isHeightPrecedencedData(h)){
-            int firstHeighest = posOf1st(heighest(h),h);
-            int lastHeighest = posOfLast(heighest(h),h);
-            int firstSecHeighest = posOf1st(secondHeighest(h),h);
-            int lastSecHeighest = posOfLast(secondHeighest(h),h);
-            if((lastSecHeighest - firstHeighest) >= (lastHeighest - firstSecHeighest)){
-                a = (lastSecHeighest - firstHeighest)*secondHeighest(h);
+        int i=0,j=h.size()-1, a=-1,n=-1;
+        while (i<j)
+        {
+            n = min(h[i],h[j])*(j-i);
+            if(n>a){
+                a = n;
+            }
+            if(h[i]<h[j]){
+                i++;
             }
             else{
-                a = (lastHeighest - firstSecHeighest)*secondHeighest(h);
+                j--;
             }
-        // }
+        }
         return a;
-    }
-
-    bool isHeightPrecedencedData(vector<int>& h){
-        return h.size() <= secondHeighest(h);
-    }
-
-    int heighest(vector<int>& h){
-        int m = 0;
-        for(int x : h){
-            m = x > m ? x : m;
-        }
-        return m;
-    }
-
-    int secondHeighest(vector<int>& h){
-        int m = heighest(h) , sM = -1;
-        for(int x : h){
-            sM = x > sM and x < m ? x : sM;
-        }
-        return sM==-1 ? m : sM;
-    }
-
-    int posOf1st(int v,vector<int>& h){
-        int p=-1;
-        for(int i=0; i < h.size(); ++i){
-            if(h[i]==v){
-                p=i;
-                break;
-            }
-        }
-        return p;
-    }
-    int posOfLast(int v,vector<int>& h){
-        int p=-1;
-        for(int i=h.size() -1; i >= 0; --i){
-            if(h[i]==v){
-                p=i;
-                break;
-            }
-        }
-        return p;
     }
 };
 int main() {
