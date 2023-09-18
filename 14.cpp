fc909c0bd2b4ece2c1d934733b4983d06d95a623
Arijit Kundu <arikundu9@gmail.com>
2023-09-18 23:38:41 +0530

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -5,6 +5,7 @@ class node {
   private:
     char data;
     vector<node *> childs;
+    bool isEnd = false;
 
   public:
     node() { data = '\0'; }
@@ -13,6 +14,7 @@ class node {
     void insertChild(string str) {
         if (str.length() <= 1) {
             node *newNode = findOrGetNew(str[0]);
+            newNode->isEnd = true;
         } else if (str.length() > 1) {
             node *newNode = findOrGetNew(str[0]);
             newNode->insertChild(str.substr(1, str.length() - 1));
@@ -30,23 +32,19 @@ class node {
         return p;
     }
 
-    void print(string ind = "\t|") {
-        cout << ind + "[+] " << data << endl;
+    void print(string ind = "|") {
+        cout << ind + "[+] " << data << (isEnd ? "[END]" : "") << endl;
         for (node *n : childs) {
             n->print(ind + "\t|");
         }
     }
 
     string getPrifix() {
-        string prefix;
+        string prefix="";
         if (childs.size() == 1) {
-            if (data != '\0') {
-                prefix = data + childs[0]->getPrifix();
-            } else {
-                prefix = childs[0]->getPrifix();
-            }
+            prefix = data + childs[0]->getPrifix();
         } else {
-            if (data != '\0') {
+            if (!isEnd) {
                 prefix = data;
             }
         }
@@ -75,19 +73,23 @@ int main() {
     Solution leetcode;
     vector<string> ip;
     string i;
-    // while (true) {
-    //     cin >> i;
-    //     if (i[0] >= 97 and i[0] <= 122) {
-    //         ip.push_back(i);
-    //         // cout<<"[--] "<<i<<endl;
-    //     } else {
-    //         break;
-    //     }
-    // }
+    while (true) {
+        cin >> i;
+        if (i[0] >= 97 and i[0] <= 122) {
+            ip.push_back(i);
+            // cout<<"[--] "<<i<<endl;
+        } else {
+            break;
+        }
+    }
 
-    ip.push_back("");
-    ip.push_back("b");
-    cout << leetcode.longestCommonPrefix(ip);
+    // ip.push_back("ab");
+    // ip.push_back("a");
+    // ip.push_back("flower");
+    // ip.push_back("flow");
+    // ip.push_back("flight");
+    // cout << leetcode.longestCommonPrefix(ip);
+    cout<<("fa"==leetcode.longestCommonPrefix(ip))<<endl;
     return 0;
 }
 
