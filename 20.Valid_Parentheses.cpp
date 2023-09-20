a89273416933278166d63d7718b5f9f57bed1f19
Arijit Kundu <arikundu9@gmail.com>
2023-09-20 22:06:33 +0530

    _____________________________

---
+++ 20.Valid_Parentheses.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    Solution() {}
+    bool isValid(string s) { return true; }
+};
+int main() {
+    Solution leetcode;
+    string ip;
+    cin >> ip;
+    cout << leetcode.isValid(ip);
+    return 0;
+}
