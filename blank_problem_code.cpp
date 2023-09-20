3935ede10a2442734bb44bc29058a436dd8e2323
Arijit Kundu <arikundu9@gmail.com>
2023-09-20 22:03:11 +0530

    blank

---
+++ blank_problem_code.cpp
@@ -0,0 +1,15 @@
+//
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    Solution() {}
+    int func(int num) { return num; }
+};
+int main() {
+    Solution leetcode;
+    int ip;
+    cin >> ip;
+    cout << leetcode.func(ip);
+    return 0;
+}
