3bf866c6fe7e95624814c3cd870af0a0cc0eeb09
Arijit Kundu <arikundu9@gmail.com>
2023-09-22 23:06:15 +0530

    format

--- 14.LongestCommonPrefix.cpp
+++ 14.LongestCommonPrefix.cpp
@@ -1,36 +1,37 @@
-//https://leetcode.com/problems/longest-common-prefix/
+// https://leetcode.com/problems/longest-common-prefix/
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-    public:
+  public:
     Solution() {}
-    string longestCommonPrefix(vector<string>& strs) {
+    string longestCommonPrefix(vector<string> &strs) {
         string prefix;
-        int horizontalItr=0;
-        bool outerLoopFlag=true, compAccumulator = true;
-        while(outerLoopFlag){
+        int horizontalItr = 0;
+        bool outerLoopFlag = true, compAccumulator = true;
+        while (outerLoopFlag) {
             compAccumulator = true;
-            for(int verticalItr=0; verticalItr<strs.size(); ++verticalItr){
-                if(strs[verticalItr].length()>1){
-                    compAccumulator = compAccumulator and (strs[verticalItr][horizontalItr]==strs[0][horizontalItr]) ;
-                    if(verticalItr==strs.size()-1){ //last iteration
-                        if(compAccumulator){
-                            prefix+=strs[0][horizontalItr];
-                        }
-                        else{
-                            outerLoopFlag=false;
+            for (int verticalItr = 0; verticalItr < strs.size();
+                 ++verticalItr) {
+                if (strs[verticalItr].length() > 1) {
+                    compAccumulator =
+                        compAccumulator and (strs[verticalItr][horizontalItr] ==
+                                             strs[0][horizontalItr]);
+                    if (verticalItr == strs.size() - 1) { // last iteration
+                        if (compAccumulator) {
+                            prefix += strs[0][horizontalItr];
+                        } else {
+                            outerLoopFlag = false;
                             break;
                         }
                     }
-                }
-                else{
-                    outerLoopFlag=false;
+                } else {
+                    outerLoopFlag = false;
                     break;
                 }
             }
             ++horizontalItr;
         }
-        if(strs.size()==1){
+        if (strs.size() == 1) {
             prefix = strs[0];
         }
         return prefix;
@@ -40,15 +41,14 @@ int main() {
     Solution leetcode;
     vector<string> ip;
     string i;
-    while(true){
-        cin>>i;
-        if(i[0]>=97 and i[0]<=122){
+    while (true) {
+        cin >> i;
+        if (i[0] >= 97 and i[0] <= 122) {
             ip.push_back(i);
-        }
-        else{
+        } else {
             break;
         }
     }
-    cout<<leetcode.longestCommonPrefix(ip);
+    cout << leetcode.longestCommonPrefix(ip);
     return 0;
 }
