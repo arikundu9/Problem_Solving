19de0368bfff48b0eff7290509067d915608a321
Arijit Kundu <arikundu9@gmail.com>
2023-09-22 11:57:43 +0530

    Create 3._Longest_Substring_Without_Repeating_Characters.cpp

---
+++ 3._Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    Solution() {}
+    int lengthOfLongestSubstring(string s) {
+        return 0;
+    }
+};
+int main() {
+    Solution leetcode;
+    string ip;
+    cin >> ip;
+    cout << leetcode.lengthOfLongestSubstring(ip);
+    return 0;
+}
