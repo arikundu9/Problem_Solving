7d793eb4a14b127419cc2b886f3e8dc3c9881d74
Arijit Kundu <arikundu9@gmail.com>
2023-09-24 01:35:26 +0530

    15.3sum=0

---
+++ 15.3sum.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/3sum/description/
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+class Solution {
+  private:
+    vector<vector<int>> triplets;
+
+  public:
+    Solution() {}
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        
+        return triplets;
+    }
+
+    friend ostream &operator<<(ostream &out, const Solution &dummy) {
+        for (vector<int> v : dummy.triplets) {
+            for (int x : v) {
+                out << x << ",";
+            }
+            out << endl;
+        }
+        return out;
+    }
+};
+int main() {
+    Solution leetcode;
+    vector<int> arg;
+    string ip, buf;
+    cin >> ip;
+    stringstream ss(ip);
+    while (getline(ss, buf, ',')) {
+        arg.push_back(stoi(buf));
+    }
+    leetcode.threeSum(arg);
+    cout << leetcode;
+    return 0;
+}
