91006ef232e7cb774bcdcbd223d2506b8ec7e951
Arijit Kundu <arikundu9@gmail.com>
2023-09-23 14:00:57 +0530

    half done, implementation to be removed

--- 3._Longest_Substring_Without_Repeating_Characters.cpp
+++ 3._Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,11 +1,21 @@
-//https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
   public:
     Solution() {}
     int lengthOfLongestSubstring(string s) {
-        return 0;
+        int windowL = 0, windowR = 0, returnLength = 0, itr = 0;
+        unordered_map<char, pair<int, int>> visitedStorage;
+        for (char c : s) {
+            visitedStorage[c] = make_pair(visitedStorage[c].first + 1, itr);
+            if (visitedStorage[c].first > 1) {
+                // repeat found.
+                returnLength = max(returnLength, itr - windowL);
+            }
+            itr++;
+        }
+        return returnLength;
     }
 };
 int main() {
