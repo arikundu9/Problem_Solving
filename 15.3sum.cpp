acece6f96bede8637ba30ddf6916d3e42df605f4
Arijit Kundu <arikundu9@gmail.com>
2023-10-02 21:39:03 +0530

    15, all valid pemutation.

--- 15.3sum.cpp
+++ 15.3sum.cpp
@@ -2,19 +2,52 @@
 #include <iostream>
 #include <sstream>
 #include <tuple>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 class Solution {
   private:
     vector<vector<int>> triplets;
+    vector<int> nums;
 
   public:
     Solution() {}
-    vector<vector<int>> threeSum(vector<int> &nums) { return triplets; }
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        this->nums = nums;
+        for (int i = 0; i < nums.size(); i++) {
+            int remainingSum = 0 - nums[i];
+            vector<vector<int>> r2s = twoSum(remainingSum, i);
+            for (vector<int> &rC : r2s) {
+                // cout << "for " << nums[i] << ",remaing=" << remainingSum
+                //      << " rsum index are:" << rC[0] << "&" << rC[1] << endl;
+                rC.push_back(i);
+                triplets.push_back({nums[rC[0]], nums[rC[1]], nums[rC[2]]});
+            }
+        }
+        return triplets;
+    }
 
-    // tuple<int, int> twoSum(int sum, int except, vector<int> nums) {
-        
-    // }
+    vector<vector<int>> twoSum(int sum, int except_pos) {
+        vector<vector<int>> rContainer;
+        unordered_map<int, int> umap;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i != except_pos) {
+                umap[nums[i]] = i;
+            }
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            int rOperand = sum - nums[i];
+            if (i != except_pos and (umap.find(rOperand) != umap.end())) {
+                if (i != umap[rOperand]) {
+                    // cout << "\tsum=" << sum << " & i=" << i
+                    //      << " & num[i]=" << nums[i]
+                    //      << " & umap.find=" << umap[rOperand] << endl;
+                    rContainer.push_back({i, umap[rOperand]});
+                }
+            }
+        }
+        return rContainer;
+    }
 
     friend ostream &operator<<(ostream &out, const Solution &dummy) {
         for (vector<int> v : dummy.triplets) {
